12a96b9edc521f6d92f37c36d0cbfdf980698498
ramonac <ramonacirlan@yahoo.com>
2024-01-02 22:48:53 +0000

    set of questions added in questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+// Create a set of questions --> array of objects
+// Each question has to contain the following:
+  // Question text
+  // Set of answers
+  //The correct answer
+
+let question1 = [
+    {
+    question:"What is the name of the first movie in the Lord of the Rings trillogy?",
+    answersabc: ["The Two Towers", "The Fellowship of the Ring", "The Return of the King"],
+    correctAnswer: "The Fellowship of the Ring"
+}];
+
+//let question2 = "Question 2 text";
+let question2 = [
+    {
+    question:"Where do the hobbits in the Lord of the Rings movie live?",
+    answersabc: ["The Shire", "Isengard", "New Zealand"],
+    correctAnswer: "The Shire"
+}];
+
+
+
+//let question3 = "Question 3 text";
+let question3 = [
+    {
+    question:"Who is the wizard who helps the hobbits in their quests?",
+    answersabc: ["Gandalf", "Saruman", "Aragorn"],
+    correctAnswer: "Gandalf"
+}];
+
+
+//let question4 = "";
+
+let question4 = [
+    {
+    question:"What was Gollum's hobbit name?",
+    answersabc: ["Sam", "Precious", "Smeagol"],
+    correctAnswer: "Smeagol"
+}];
+
+//let question5 = "";
+
+let question5 = [
+    {
+    question:"Who was the director for The Lord of the Rings trilogy?",
+    answersabc: ["James Cameron", "Steven Spielberg", "Peter Jackson"],
+    correctAnswer: "Peter Jackson"
+}];
